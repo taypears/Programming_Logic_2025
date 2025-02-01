504186fd0775d7589f2b345fecc7d3bb801c5509
taypears <p.tayeja@gmail.com>
2025-02-01 16:37:32 -0500

    Adding for programming logic class lab 1

---
+++ Lab 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+         Console.WriteLine("TayeJa Pearson S01322082@acad.tri-c.edu");/*2. TayeJa Pearson
+        S01322082@acad.tri-c.edu
+        */
+        Console.WriteLine("IT 1050 CRN 13078"); //3. IT 1050 CRN 13078//
+        int myNum = 1; //4. number of times I've written code//
+        Console.WriteLine('myNum')
+        string x = "3.14159"; //5. store the value of pi in a variable "x"//
+        Console.WriteLine(x);
+        //6. create a variable to store a character and assign your favorite letter of the alphabet to it. output the value.
+        char myChar = 'T';
+        Console.WriteLine(T);
+
+    }
+}
