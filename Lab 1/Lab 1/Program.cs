80e53200b29574ac913a05735af7371764f44a89
taypears <p.tayeja@gmail.com>
2025-02-07 20:52:26 -0500

    added lab 2

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1.Include the instructions for each step as a comment in your code.
+
+        /*2. Add a multi-line comment with your name and email address.
+            TayeJa Pearson S01322082@acad.tri-c.edu*/
+            Console.WriteLine("TayeJa Pearson S01322082@acad.tri-c.edu");
+
+        //3. [IT 1050 CRN 13078] Add a single line comment with the course number and CRN for this course to your lab one code.
+            Console.WriteLine("IT 1050 CRN 13078"); 
+
+        //4. Create an integer variable for the number of times you have written code in any programming language. 
+            int myNum = 1; 
+            Console.WriteLine(myNum);
+
+        //5. Create a variable to store the value of pi which is 3.14159 and output the value.
+            string x = "3.14159"; 
+            Console.WriteLine(x);
+            
+        //6. create a variable to store a character and assign your favorite letter of the alphabet to it. Output the value.
+            char myLetter = 'T';
+            Console.WriteLine(myLetter);
+    }
+}
---
+++ Lab 1/Lab 2/Lab2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //1.Include the instructions for each step as a comment in your code.
+
+        /*2. Add a multi-line comment with your name, email address course number and CRN.
+            TayeJa Pearson, S01322082@acad.tri-c.edu, 1050, 13078*/
+              Console.WriteLine("TayeJa Pearson S01322082@acad.tri-c.edu 1050 13078");
+
+        //3. Needs more: Create an integer variable to store a number that represents something significant to and assign it a number
+
+        //4. Create a string variable and assign a value of your choosing 
+
+        //5. Create a boolean variable ...
+
+        /*6. In a multi-line comment state what happens in a C# program when C# syntax is incorrectly used. 
+            In your response, state exactly what is wrong with each line. Use the following code (Make sure you run the code)  
+
+
+            If there are syntax errors, it is possible that the code will not run at all. 
+            Since C# is case-sensitive, if a single element is not capitalized it can cause a syntax error.
+            Also shown below, if quotations and parenthesis aren't used appropriately or if the ending semicolon is missing then it could also cause an error in the code as shown below.
+            These errors could cause the code to be non-functional.
+            
+            console.WriteLine("Hello World!"); -> The "C" is not capitalizd in Console
+            Console.Writeline("First C# code!"); -> The "L" is not capitalizd in WriteLine
+            Console.WriteLine(Hello IT1050 Students!"); -> The beginning quote is missing in the parenthesis before the word "hello"
+      */
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First C# code!");
+            Console.WriteLine("Hello IT1050 Students!");
+    }
+}
