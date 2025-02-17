d7c3f46ff820133103450f991bd1e649d5049302
taypears <p.tayeja@gmail.com>
2025-02-17 00:12:47 -0500

    extra

--- Lab 1/Lab 2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
+++ Lab 1/Lab 2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95652dbf72f2424b9cfac375355042234c3fa534")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8403ed354299a69671d4332405f439abf3294c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
