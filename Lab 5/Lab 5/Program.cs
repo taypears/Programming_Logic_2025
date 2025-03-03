afec70809be9696331af6000672ff6112df9f092
taypears <p.tayeja@gmail.com>
2025-03-03 14:55:15 +0000

    added baseline questions

--- Lab 5/Lab 5/Program.cs
+++ Lab 5/Lab 5/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        /* 1. Include your name, course number and CRN as a comment in your code.
+                TayeJa Pearson [IT 1050 CRN 13078]*/
+        
+        /* 2. Include the instructions for each step as a comment in your code.*/
+
+        /* 3. Given 2 integer variables temp1= 53 and temp2 = 25, use an if statement to output
+                 “COLD WEATHER!” if temp2 is less than temp1.*/
+
+        /* 4. Create a Boolean variable named checkAge, assign a value to it and use an if-else 
+                statement to determine if a person is an adult or not. If checkAge is true, output 
+                "The person is an adult." If checkAge is false, output "The person is not an adult." */
+
+        /* 5. Use a switch statement to output the following values for swimming course level description.
+                Level 1 - Introduction to Water Skills
+                Level 2 - Fundamental Aquatic Skills
+                Level 3 - Stroke Development
+                Level 4 – Stroke Improvement
+                Level 5 – Stroke Refinement
+                Level 6 - Swimming and Skill Proficiency*/
+
+        /* 6. Convert the switch statement in step 5 to an if-else if-else statement.*/
+        
+
     }
 }
