8d643f403ae8ddc42dfbe7726cef7af3dfdef7c3
taypears <p.tayeja@gmail.com>
2025-03-03 15:53:00 +0000

    a lil something for all q's

--- Lab 5/Lab 5/Program.cs
+++ Lab 5/Lab 5/Program.cs
@@ -23,6 +23,13 @@ class Program
         /* 4. Create a Boolean variable named checkAge, assign a value to it and use an if-else 
                 statement to determine if a person is an adult or not. If checkAge is true, output 
                 "The person is an adult." If checkAge is false, output "The person is not an adult." */
+                
+                 bool checkAge;
+                 checkAge = true;
+                        if (checkAge)
+                         { Console.WriteLine("The person is an adult.");}
+                        else
+                         { Console.WriteLine("The person is not an adult.");}
 
         /* 5. Use a switch statement to output the following values for swimming course level description.
                 Level 1 - Introduction to Water Skills
@@ -32,8 +39,36 @@ class Program
                 Level 5 – Stroke Refinement
                 Level 6 - Swimming and Skill Proficiency*/
 
-        /* 6. Convert the switch statement in step 5 to an if-else if-else statement.*/
+                
 
+        /* 6. Convert the switch statement in step 5 to an if-else if-else statement.
+                ***This code may be right ... DOUBLE CHECK!! */
+                int carSpeed = 102;
+
+            if (carSpeed >= 130 && carSpeed <= 200)
+            {
+                Console.WriteLine("Accident");
+            }
+            else if (carSpeed >= 110 && carSpeed <= 129)
+            {
+                Console.WriteLine("Police");
+            }
+            else if (carSpeed >= 100 && carSpeed <= 109)
+            {
+                Console.WriteLine("Casual Highway");
+            }
+            else if (carSpeed >= 50 && carSpeed <= 99)
+            {
+                Console.WriteLine("Just OK");
+            }
+            else if (carSpeed >= 0 && carSpeed <= 49)
+            {
+                Console.WriteLine("Slow Poke");
+            }
+            else
+            {
+                Console.WriteLine("Out of Range");
+            }
 
     }
 }
