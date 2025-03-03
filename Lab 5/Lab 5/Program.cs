c65e433f06f60af9b2615474d5eb6af0c0dd8f7e
taypears <p.tayeja@gmail.com>
2025-03-03 15:02:02 +0000

    q3 update

--- Lab 5/Lab 5/Program.cs
+++ Lab 5/Lab 5/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
         /* 1. Include your name, course number and CRN as a comment in your code.
                 TayeJa Pearson [IT 1050 CRN 13078]*/
         
@@ -12,6 +13,12 @@ class Program
 
         /* 3. Given 2 integer variables temp1= 53 and temp2 = 25, use an if statement to output
                  “COLD WEATHER!” if temp2 is less than temp1.*/
+                
+                int temp1 = 53;
+                int temp2 = 25;
+
+                if (temp2 < temp1)
+                    {Console.WriteLine ("COLD WEATHER!")};
 
         /* 4. Create a Boolean variable named checkAge, assign a value to it and use an if-else 
                 statement to determine if a person is an adult or not. If checkAge is true, output 
@@ -26,7 +33,7 @@ class Program
                 Level 6 - Swimming and Skill Proficiency*/
 
         /* 6. Convert the switch statement in step 5 to an if-else if-else statement.*/
-        
+
 
     }
 }
