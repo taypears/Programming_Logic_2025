33fe2a8e495c67ed931bfcf58cc5f1c840c8f6af
taypears <p.tayeja@gmail.com>
2025-03-03 22:35:55 -0500

    update to all

--- Lab 1/Lab 2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
+++ Lab 1/Lab 2/Lab2/obj/Debug/net9.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54a43dc975ea7a76be908860029519052110bf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+809597b743bce39fe902e9048d8734ff6e275c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
