141b8ea73369f35151081462e8d6f2d186350f6d
taypears <p.tayeja@gmail.com>
2025-03-09 18:17:47 -0400

    final touches

--- Lab6/Lab 6/obj/Debug/net9.0/Lab 6.AssemblyInfo.cs
+++ Lab6/Lab 6/obj/Debug/net9.0/Lab 6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab 6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721bf63bdd4f842705ab70c4eefdb0594b70c18a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef624367bd7fe4691d95f9cb816afd57c052e1ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab 6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab 6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
