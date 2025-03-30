0d74f0a3e7cbf4a25df7bb2fae751308be1cf51f
taypears <p.tayeja@gmail.com>
2025-03-29 21:12:20 -0400

    7 update

--- Lab 3/lab 3/obj/Debug/net9.0/lab 3.AssemblyInfo.cs
+++ Lab 3/lab 3/obj/Debug/net9.0/lab 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524c87b6a10803f3d871dbe26bb8dda16ce10cc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c22dd35d0be360f751bfda3c4bb21de2db6925")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab8/lab8/obj/Debug/net9.0/lab8.AssemblyInfo.cs
+++ Lab8/lab8/obj/Debug/net9.0/lab8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87511781fb4c1e4ca7de19b702e01b972272ad37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c22dd35d0be360f751bfda3c4bb21de2db6925")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
