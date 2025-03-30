d204e1a30f65fb2e5934d70a40d25ca3e11a3152
taypears <p.tayeja@gmail.com>
2025-03-30 01:47:57 -0400

    finished product, just needs to be tested again

--- Lab8/lab8/Program.cs
+++ Lab8/lab8/Program.cs
@@ -4,12 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         /*1. Include your name, course number and CRN as a comment in your code.*/ 
             //TayeJa Pearson [IT1050 CRN 13078] 
 
-        /* 2. Include the instructions for each step as a comment in your code.*/ 
+        /*2. Include the instructions for each step as a comment in your code.*/ 
 
         /*3. Use a while loop to print the first eight multiples of 5 all on one line with one space between them for readability. Make sure that the output for the next step does not combine with the output for step 3.*/ 
 
@@ -55,32 +53,8 @@ class Program
             Console.WriteLine (" "); 
             } 
 
-        /*7a. Use a while loop to accomplish the following task - use a 5-digit integer variable to store a password and assign a value to it. Then prompt a user to enter the password which must match the stored password. The user has only three tries to match the stored password. If the user correctly matches the stored password, print a message letting the user know they entered the correct password. You must also let the user know how many tries they have remaining. After three tries of incorrectly matching the stored password, print a message letting the user know that there are no more chances to try to match the password. */ 
-
-
-            // int Pwd = 51997; //sets password to 51997
-            // int attempt = 3; //sets number of attempts to 3
-           
-            //     Console.WriteLine("Please enter your password:");
-            //     int userInput = Convert.ToInt32 (Console.ReadLine ());
-            //     while (attempt > 0) //while loop that runs while attempts are greater than 0
-            //         {
-            //         if (userInput == Pwd) //checks if user input matches password
-            //             { 
-            //             Console.WriteLine("The password you entered is correct."); 
-            //             break; 
-            //             } 
-            //         else 
-            //             { 
-            //             attempt--; //Decrements remaining attempts and prints remaining amount of attempts to the screen
-            //             Console.WriteLine("Invalid Input. You have " + attempt + " attempts remaining. **Hint, it is a 5 digit number**"); 
-            //             }
-            //         if (attempt == 0) //Conditions for when attempts reach 0
-            //             {
-            //             Console.WriteLine("You have zero remaining attempts. Your account is now locked. Please contact your administrator to regain access."); 
-            //             }
-            //         }
-        /*7b. Use a while loop to accomplish the following task - use a 5-digit integer variable to store a password and assign a value to it. Then prompt a user to enter the password which must match the stored password. The user has only three tries to match the stored password. If the user correctly matches the stored password, print a message letting the user know they entered the correct password. You must also let the user know how many tries they have remaining. After three tries of incorrectly matching the stored password, print a message letting the user know that there are no more chances to try to match the password. */ 
+    
+        /*7. Use a while loop to accomplish the following task - use a 5-digit integer variable to store a password and assign a value to it. Then prompt a user to enter the password which must match the stored password. The user has only three tries to match the stored password. If the user correctly matches the stored password, print a message letting the user know they entered the correct password. You must also let the user know how many tries they have remaining. After three tries of incorrectly matching the stored password, print a message letting the user know that there are no more chances to try to match the password. */ 
             
                 int Pwd1 = 51997; //sets password to 51997
                 bool userInp = false;
@@ -98,14 +72,20 @@ class Program
                         }
                     else 
                         {
-                            for (counter=3; counter> 0;) 
+                            if (counter > 1) 
                                 {
                                 counter--;
                                 Console.WriteLine ("Invalid input. You have " + counter + " attempts remaining. **Hint, it is a 5 digit number**");
+                                //break;
+                                }
+                            else
+                                {
+                                Console.WriteLine ("You have exceeded the maximum amount of attempts. your account is now locked. Please contact your administrator to reset password");
                                 break;
                                 }
                         }
-                }
+                    }
+    
 
     }
 }
