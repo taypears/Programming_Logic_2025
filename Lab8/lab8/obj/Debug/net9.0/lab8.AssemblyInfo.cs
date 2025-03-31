36d829b4ab26b2410b5e368f7ae4ade48ab0a4fd
taypears <p.tayeja@gmail.com>
2025-03-31 10:12:02 -0400

    debugger ran

--- Lab8/lab8/obj/Debug/net9.0/lab8.AssemblyInfo.cs
+++ Lab8/lab8/obj/Debug/net9.0/lab8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0f6379a32436e5ebbea8a3dd3d189d9a4554ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7af6e6437b072905dc0b9ee9c73b70835d7a92")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
