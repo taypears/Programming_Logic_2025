982a1c7b2e9ecb309acdc15993e405c9a0d75d58
taypears <p.tayeja@gmail.com>
2025-03-31 10:31:18 -0400

    standard line removed and spacing added

--- Lab9/Lab9/Program.cs
+++ Lab9/Lab9/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
 
        /*1. Include your name, course number and CRN as a comment in your code.*/ 
             //TayeJa Pearson [IT1050 CRN 13078] 
@@ -13,9 +12,11 @@ class Program
 
         /* 3. Create an array to store six string values of courses that you are currently taking or planning to take for your degree. Give your array a descriptive name and print out the array values on separate lines either using a for loop or a foreach loop. */
 
-        
+
 
         /* 4. Create an array to store 8 integer values that represent the different ages of people in a community. Give your array a descriptive name and print out the array values on one line with a space between the values using either a for loop or a foreach loop to print out the values. Make sure that your output is readable. */
+
+
 
         /* 5. Create an array to store 7 different temperature values that represent winter temperatures in the Cleveland area. Give your array a descriptive name. Print the array values and the average of the 7 temperature values. */
         /* 6. Create parallel arrays for the following product names and prices. Use Products for the first array's name and Prices for the second array's name. Output the two together using a loop.
@@ -29,6 +30,7 @@ class Program
                     Milk               2.60
                     Paper Towels 2.58
                     Bagels            3.97 */
+
 
 
         /* 7. Given the following 2D array, int [ , ] StudentScores = { {75, 82, 90}, {88, 89, 92}, {65, 70, 74} }; print out the values of the array so that they display in a table format as shown below:  
