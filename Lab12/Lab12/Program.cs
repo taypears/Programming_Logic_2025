27f745ad9db42a8d99041bdd6a96e172dba37520
taypears <p.tayeja@gmail.com>
2025-04-28 11:22:04 -0400

    lab 12 questions added

---
+++ Lab12/Lab12/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        /*1.   Include your name, course number and course CRN as a comment in your code.*/
+                //TayeJa Pearson [IT 1050 CRN 13078]
+
+        /*2.   Include the instructions for each step as a comment in your code.*/
+
+        /*3.   Use the following given code to demonstrate exception handling. Run the code first to see the unhandled exception message and then add exception handling code to ensure that the line Console.WriteLine(Numbers[6]); does not crash the program. The Numbers array is a 6-integer array and defined as follows:
+        int [ ] Numbers = { 23, 45, 76, 89, 12, 37};    //declare array 
+        for (int i = 0; i < Numbers.Length; i++) {       //print out array values
+                    Console.WriteLine(Numbers[i]);
+                }
+        Console.WriteLine(Numbers[6]);     //try to access invalid index of array [Note - To ensure that your code runs, put the given code and your exception handling code in the Main method of your C# program]*/
+
+                int [ ] Numbers = { 23, 45, 76, 89, 12, 37};    //declare array 
+                for (int i = 0; i < Numbers.Length; i++) 
+                    {       //print out array values
+                    Console.WriteLine(Numbers[i]);
+                    }
+
+        /*4.  Using the class that you created in Lab 11, add two more fields to your class and use the private access modifier for these new fields. Add a default constructor that does not accept any parameters and sets the value of all four fields with default (initial) values. Modify the void method in your class that prints the class details so that it will print all four fields.*/
+
+        /*5.   Add a constructor method to your class that accepts 2 of the 4 parameters. */
+
+        /*6.   Add a constructor method to your class that accepts all four parameters. */
+
+        /*7.  Create an object of your class and call the 2-parameter constructor to initialize 2 of the fields and output the object details.*/
+
+        /*8.   Create a second object of your class and call the 4-parameter constructor to initialize all 4 fields and output the object details. */
+    }
+}
