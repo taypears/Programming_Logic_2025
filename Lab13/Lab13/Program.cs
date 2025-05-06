de5a9ff53174b67faf2472c42251c97dfc2dd69a
taypears <p.tayeja@gmail.com>
2025-05-05 21:08:51 -0400

    lab 13 qs added

---
+++ Lab13/Lab13/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*1.  Include your name, course number and course CRN as a comment in your code. /*
+            //TayeJa Pearson [IT 1050 CRN 13078]
+
+        /*2.  Include the instructions for each step as a comment in your code. /*
+
+        /*3.  Given the following class Triangle as defined in the provided code Lab13.txt, create a derived (child) class called Equilateral. Add a method called displayTriangleInfo() that outputs the following statement “Equilateral triangles have 3 equal sides.”/*
+            
+        /*4. Create a derived (child) class called Isosceles. Add a method called displayTriangleInfo() that outputs the following statement “Isosceles triangles have 2 equal sides.” /*
+
+        /*5.  Create a derived (child) class called Scalene. Add a method called displayTriangleInfo() that outputs the following statement “Scalene triangles have no equal sides.” /*
+
+        /*6.  Create an object of type Equilateral. Call its displayTriangleInfo() method. /*
+
+        /*7.  Create an object of type Isosceles. Call its displayTriangleInfo() method. /*
+
+        /*8.  Create an object of type Scalene. Call its displayTriangleInfo() method./*
+    }
+}
