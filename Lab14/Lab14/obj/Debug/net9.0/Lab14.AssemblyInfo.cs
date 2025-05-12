ea89a6146ae10a048af11661f25427ba3bc9ba00
taypears <p.tayeja@gmail.com>
2025-05-11 22:41:09 -0400

    try 1

--- Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
+++ Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef58ba7e15de0c8eb120549fdce521f63b87e5fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d12e9bfbc1cc6853c1fcee6ff946bb74376e6f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
