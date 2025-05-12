d6c63a683bbb6cf827e2756d62dd2faaf6d6f3a0
taypears <p.tayeja@gmail.com>
2025-05-11 22:44:38 -0400

    diff added

--- Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
+++ Lab14/Lab14/obj/Debug/net9.0/Lab14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea89a6146ae10a048af11661f25427ba3bc9ba00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1acef9bd280ac7bcf608801f05fd97056f8eac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
